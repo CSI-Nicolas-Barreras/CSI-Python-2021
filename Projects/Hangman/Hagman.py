9105f578ed8a223a3d903553356334aa8178e697
CSI-Nicolas-Barreras <24-007@sanignacio.pr>
2022-02-16 10:49:38 -0400

    clase febreo16

--- Projects/Hangman/Hagman.py
+++ Projects/Hangman/Hagman.py
@@ -3,17 +3,18 @@ import urllib, json
 from RandomFood import RandomFood
 import urllib.request
 
-# This is discouraged but it will avoid certificate validation (prevents error)
-ssl._create_default_https_context = ssl._create_unverified_context
+def get_food():
+    # This is discouraged but it will avoid certificate validation (prevents error)
+    ssl._create_default_https_context = ssl._create_unverified_context
 
-foodsURL = "https://random-data-api.com/api/food/random_food"
-    
-req = urllib.request.Request(foodsURL)
-requestData = json.loads(urllib.request.urlopen(req).read())
+    foodsURL = "https://random-data-api.com/api/food/random_food"
+        
+    req = urllib.request.Request(foodsURL)
+    requestData = json.loads(urllib.request.urlopen(req).read())
 
-food:RandomFood = RandomFood(**requestData)
+    food:RandomFood = RandomFood(**requestData)
 
-print (food.ingredient)
+    return (food.ingredient)
 
 Steps= ["""
         /---------|
@@ -93,4 +94,21 @@ Steps= ["""
 for step in Steps:
     print (step)
 
-    
+
+def getInput():
+    invalid_characters= ("1","2","3","4","5","6","7","8","9","10",",","'","/","?","<",":",";",">",".","[","]","{","}","|","=","+","_","-",")","0","(","*","&","^","%","$","#","@","!","`","~")
+   
+    while(True):   
+        letter = input("Choose letter")
+
+        if(len(letter)!= 1):
+            print("error")
+            continue
+
+        if(letter in invalid_characters):
+            print("error")
+            continue
+
+        return letter
+
+print(getInput())

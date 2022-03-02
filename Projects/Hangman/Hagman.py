cce78ed8227b2b834fc30a32e7a76dd31c301aa0
CSI-Nicolas-Barreras <24-007@sanignacio.pr>
2022-03-01 22:26:17 -0400

    Proyecto

--- Projects/Hangman/Hagman.py
+++ Projects/Hangman/Hagman.py
@@ -1,21 +1,36 @@
+
 import ssl
 import urllib, json
 from RandomFood import RandomFood
 import urllib.request
 
+# used characters list
+used=[]
+
+# Giving a function to this code
 def get_food():
-    # This is discouraged but it will avoid certificate validation (prevents error)
+
+# This is discouraged but it will avoid certificate validation (prevents error)
     ssl._create_default_https_context = ssl._create_unverified_context
 
+# This is my URL from the Api provider
     foodsURL = "https://random-data-api.com/api/food/random_food"
-        
+
+# Request from URL to choose word    
     req = urllib.request.Request(foodsURL)
     requestData = json.loads(urllib.request.urlopen(req).read())
-
     food:RandomFood = RandomFood(**requestData)
 
-    return (food.ingredient)
+    return food.ingredient.upper()
 
+#Create variable to store the value of get_food()
+myFood= get_food()
+
+WrongLetters = []
+
+print(len(myFood) * "_")
+
+# Steps for Hangman
 Steps= ["""
         /---------|
         |         |
@@ -91,15 +106,13 @@ Steps= ["""
     ------------------- 
 """,
 ]
-for step in Steps:
-    print (step)
 
 
 def getInput():
     invalid_characters= ("1","2","3","4","5","6","7","8","9","10",",","'","/","?","<",":",";",">",".","[","]","{","}","|","=","+","_","-",")","0","(","*","&","^","%","$","#","@","!","`","~")
    
     while(True):   
-        letter = input("Choose letter")
+        letter = input("Choose letter=").upper()
 
         if(len(letter)!= 1):
             print("error")
@@ -109,6 +122,50 @@ def getInput():
             print("error")
             continue
 
+        if letter in WrongLetters:
+            print("Already guessed letter")
+
+        used.append(letter)
+
         return letter
 
-print(getInput())
+
+
+def printWord():
+    temp:str=""
+
+    for letter in myFood:
+        if letter in used:
+            temp= temp + letter
+        else: 
+            temp= temp + "_"
+        
+
+
+    print(temp)
+
+    
+
+def printStep():
+    
+    counter= 0
+    for letter in used:
+        if letter not in myFood:
+            counter= counter + 1
+
+    print(Steps[counter])
+
+
+    
+    
+while True:
+     printStep()
+     getInput()
+     printWord()
+
+        
+    
+    
+
+  
+

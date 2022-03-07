98d389d89b6620d6430673c9a17b69ed17a36560
CSI-Nicolas-Barreras <24-007@sanignacio.pr>
2022-03-07 13:25:36 -0400

    Proyecto terminado

--- Projects/Hangman/Hagman.py
+++ Projects/Hangman/Hagman.py
@@ -1,20 +1,17 @@
 
+from random import gammavariate
 import ssl
 import urllib, json
 from RandomFood import RandomFood
 import urllib.request
 
 # used characters list
-used=[]
+
 
 # Giving a function to this code
 def get_food():
-
-# This is discouraged but it will avoid certificate validation (prevents error)
-    ssl._create_default_https_context = ssl._create_unverified_context
-
-# This is my URL from the Api provider
-    foodsURL = "https://random-data-api.com/api/food/random_food"
+    ssl._create_default_https_context = ssl._create_unverified_context  # This is discouraged but it will avoid certificate validation (prevents error)
+    foodsURL = "https://random-data-api.com/api/food/random_food"   # This is my URL from the Api provider
 
 # Request from URL to choose word    
     req = urllib.request.Request(foodsURL)
@@ -22,13 +19,6 @@ def get_food():
     food:RandomFood = RandomFood(**requestData)
 
     return food.ingredient.upper()
-
-#Create variable to store the value of get_food()
-myFood= get_food()
-
-WrongLetters = []
-
-print(len(myFood) * "_")
 
 # Steps for Hangman
 Steps= ["""
@@ -107,65 +97,79 @@ Steps= ["""
 """,
 ]
 
-
+#Giving a function to this code
 def getInput():
-    invalid_characters= ("1","2","3","4","5","6","7","8","9","10",",","'","/","?","<",":",";",">",".","[","]","{","}","|","=","+","_","-",")","0","(","*","&","^","%","$","#","@","!","`","~")
-   
+    invalid_characters=["""
+    1""","""2""","""3""",   #list of invalid characters
+    """4""","""5""","""6""",
+    """7""","""8""","""9""",
+    """10""",""",""","""'""",
+    """/""","""?""","""<""",
+    """:""",""";""",""">""",
+    """.""","""[""","""]""",
+    """{""","""}""","""|""",
+    """=""","""+""","""_""",
+    """-""",""")""","""0""",
+    """(""","""*""","""&""",
+    """^""","""%""","""$""",
+    """#""","""@""","""!""",
+    """`""","""~""",
+    ]  
     while(True):   
+        #prints in the game "Choose Letter=" for your guess
         letter = input("Choose letter=").upper()
 
         if(len(letter)!= 1):
             print("error")
             continue
-
+        # for a guess in the list of invalid characters prints error
         if(letter in invalid_characters):
             print("error")
             continue
-
-        if letter in WrongLetters:
+        # if you guesed the same word again it prints already guessed letter
+        if letter in used:
             print("Already guessed letter")
 
         used.append(letter)
 
         return letter
 
-
-
+#Giving a function to this code
 def printWord():
     temp:str=""
 
-    for letter in myFood:
-        if letter in used:
-            temp= temp + letter
-        else: 
-            temp= temp + "_"
-        
-
-
+    for letter in myFood:   # this checks in if the letter is in myFood
+        if letter in used:  # this tells the computer that is the letter is in the list of used
+            temp= temp + letter # temp is
+        else: temp= temp + "_"
     print(temp)
+    return temp
 
-    
-
+#Giving a function to this code
 def printStep():
-    
-    counter= 0
-    for letter in used:
-        if letter not in myFood:
-            counter= counter + 1
+    counter= 0  # keeps track of the steps you are
+    for letter in used: #this checks if the letter is in used 
+        if letter not in myFood: #this tells the computer that the word is not in myFood that is the letter
+            counter= counter + 1 #this add a step forevery error
+    print(Steps[counter])   #this prints the steps
+    return counter  #this sends this code to whiletrue/ print steps
 
-    print(Steps[counter])
-
-
-    
-    
 while True:
-     printStep()
-     getInput()
-     printWord()
+    myFood = get_food() # this place the word in get food
+    used = [] # this the used list
+       
+    while True:
+        counter = printStep() # this is the return 
+        getInput()
+        temp = printWord()
+        usedString = str(used).replace('\'','').replace('[','').replace(']','')# this is from planets.py this eliminates all the brackets,qoutes and replace it with nothing
+        print(f"Used Letters: {usedString}")    #this prints Used Letter for keeping a list and indicate you for the word that you already used
 
-        
-    
-    
+#this tells us if we won or lost the game
+        if(temp == myFood):   #
+            print ("G A M E  W O N")  # prints the string
+            break   #reset the game
 
-  
-
+        if(counter == 7):   # it counts the steps until you get to the step 7 so it can print Game Over
+            print("G A M E  O V E R ")  # prints the string
+            break   #reset the game

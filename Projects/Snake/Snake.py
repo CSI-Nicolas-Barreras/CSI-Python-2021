9747bed7e0a17d1751defaa71b82aa3e7fa0f988
CSI-Nicolas-Barreras <24-007@sanignacio.pr>
2022-03-09 10:14:42 -0400

    clase

--- Projects/Snake/Snake.py
+++ Projects/Snake/Snake.py
@@ -1,1 +1,12 @@
-[Snake](https://www.edureka.co/blog/snake-game-with-pygame/)
+#imports the libary that we downloaded for games.
+import pygame 
+# initialize all imported pygame modules.
+pygame.init()
+# creates an instance of the pygame.
+dis=pygame.display.set_mode((400,300))
+# updates the entire Surface, only if no argument is passed. 
+pygame.display.update()
+# shut down pygame.
+pygame.quit()
+# it terminates the execution of the program completely.
+quit()

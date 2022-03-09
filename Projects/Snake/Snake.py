bdb969d280e7ec51d58a4e9af7135fec67bd8fbd
CSI-Nicolas-Barreras <24-007@sanignacio.pr>
2022-03-09 10:23:44 -0400

    clase

--- Projects/Snake/Snake.py
+++ Projects/Snake/Snake.py
@@ -6,6 +6,16 @@ pygame.init()
 dis=pygame.display.set_mode((400,300))
 # updates the entire Surface, only if no argument is passed. 
 pygame.display.update()
+# Snake Game By Nicolas Barreras
+pygame.display.set_caption('Snake game by Edureka')
+#created a variable game over that is a bolean that is false
+game_over=False
+#loop run while the game is not over
+while not game_over:
+    #no c
+    for event in pygame.event.get():
+        #prints out all the actions that take place on the screen
+        print(event)   
 # shut down pygame.
 pygame.quit()
 # it terminates the execution of the program completely.
